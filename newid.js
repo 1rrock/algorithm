3c9407e51d6a0ebf4d53bfe3d87d54b71ea9e7b9
1rrock <zxcv1685@gmail.com>
2022-07-19 15:09:14 +0900

    report

---
+++ newid.js
@@ -0,0 +1,16 @@
+function solution(new_id) {
+    let answer = new_id.toLowerCase();
+    answer = answer.replace(/[^a-z0-9-_.]/gi, "");
+    answer = answer.replace(/\.{2,}/gi, ".");
+    answer = answer[0] == "." ? answer.substring(1) : answer;
+    answer = answer.slice(0, 15);
+    answer = answer[answer.length - 1] == "." ? answer.slice(0, -1) : answer;
+    answer = answer === "" ? "a" : answer;
+    if (answer.length <= 2) {
+        while (answer.length < 3) {
+            answer += answer[answer.length - 1];
+        }
+    }
+    return answer;
+}
+console.log(solution("...!@BaT#*..y.abcdefghi-jklm..."));
